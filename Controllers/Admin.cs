c32797ed0fd540076ebcf4242eac4c9279d93e05
Uzair <bcsf20m014@pucit.edu.pk>
2023-06-11 13:30:09 +0500

    missing files added

---
+++ Controllers/Admin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HarborView_Inn.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult admin()
+        {
+            return View();
+        }
+    }
+}
